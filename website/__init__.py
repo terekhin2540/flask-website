0b60084f3d4c76c53b8d9d220ee178e19132f72a
danisimmo99 <danisimmo99@mail.ru>
2022-05-25 02:05:59 +0300

    first commit

---
+++ website/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloword"
+
+    
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
+
+    
