5f49879b142ed95fbd4f6a1b3e77f80e58174e2d
danisimmo99 <danisimmo99@mail.ru>
2022-05-29 02:48:08 +0300

    добавил navbar наверху страниц и настроил редиректы

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
-    return 'login'
+    return render_template('login.html')
 
 
 @auth.route('/sign-up')
 def sign_up():
-    return 'sign-up' 
+    return render_template('signup.html') 
 
 
 @auth.route('/logout')
 def logout():
-    return 'logout'
+    return redirect(url_for('views.home'))
+    
